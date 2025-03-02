0634636db110a69c87f4376acd6ba7625e10de8a
“Shiv <stavangandhi3008@gmail.com>
2025-03-02 01:17:53 -0600

    Initial Commit

--- Mitr/MitrApp.swift
+++ Mitr/MitrApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct MitrApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.portrait
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
